b1e9406c633492a732b6d4857ef52e14e3109dce
alantao5056 <alantao5056@outlook.com>
2021-04-14 09:07:07 -0400

    finished gates

---
+++ 2016/January/gates/gates.py
@@ -0,0 +1,56 @@
+def getCord(d, curCord):
+  if d == "N":
+    return (curCord[0], curCord[1] + 1,)
+  elif d == "E":
+    return (curCord[0] + 1, curCord[1],)
+  elif d == "S":
+    return (curCord[0], curCord[1] - 1,)
+  else:
+    # curCord is "W"
+    return (curCord[0] - 1, curCord[1],)
+  
+def getNumOfGates(N, directions):
+  count = 0
+  visitedDirec = set([(0, 0,)])
+  fences = {}
+  lastCord = (0, 0)
+  
+  curCord = (0, 0)
+  
+  for i in range(0, N):
+    curCord = getCord(directions[i], curCord) # updates curCord
+    
+    if curCord in fences:
+      fences[curCord].add(lastCord)
+    else:
+      fences[curCord] = set([lastCord])
+
+
+    if curCord in visitedDirec:
+      if curCord not in fences[lastCord]:
+        count += 1
+
+    if lastCord in fences:
+      fences[lastCord].add(curCord)    
+    else:
+      fences[lastCord] = set([curCord])
+
+    visitedDirec.add(curCord)
+    lastCord = curCord
+
+  return count
+
+
+def main(inputFile, outputFile):
+  gatesInput = open(inputFile, 'r')
+  N = int(gatesInput.readline().strip())
+  directions = gatesInput.readline().strip()
+  gatesInput.close()
+  
+  
+  gatesOutput = open(outputFile, 'w')
+  gatesOutput.write(str(getNumOfGates(N, directions)) + '\n')
+  gatesOutput.close()
+
+
+main('gates.in', 'gates.out')
