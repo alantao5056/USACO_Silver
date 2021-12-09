c07aa9ee46e6cc4e4072d3c86b2459de52e69b32
Alan Tao <47901465+alantao5056@users.noreply.github.com>
2021-12-09 15:28:45 -0500

    finished comfortable (#34)

---
+++ 2021/February/comfortable/Comfortable.java
@@ -0,0 +1,113 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Comfortable {
+  public static boolean[][] grid;
+  public static void main(String[] args) throws Exception {
+    // Scanner sc = new Scanner(new java.io.File("comfortable.in"));
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    grid = new boolean[2000][2000];
+    int total = 0;
+
+    for (int i = 0; i < N; i++) {
+      int x = sc.nextInt() + 500;
+      int y = sc.nextInt() + 500;
+
+      if (!grid[x][y]) {grid[x][y] = true;}
+      else total--;
+
+      // add the cows
+      Queue<Integer> xq = new LinkedList<>();
+      Queue<Integer> yq = new LinkedList<>();
+
+      xq.add(x);
+      yq.add(y);
+
+
+      while (!xq.isEmpty()) {
+        int curX = xq.poll();
+        int curY = yq.poll();
+
+        int[] next = isComfortable(curX, curY);
+        if (next != null) {
+          total++;
+          int nextX = next[0];
+          int nextY = next[1];
+          xq.add(nextX);
+          yq.add(nextY);
+          grid[nextX][nextY] = true;
+        }
+
+        next = isComfortable(curX, curY+1);
+        if (next != null) {
+          total++;
+          int nextX = next[0];
+          int nextY = next[1];
+          xq.add(nextX);
+          yq.add(nextY);
+          grid[nextX][nextY] = true;
+        }
+
+        next = isComfortable(curX, curY-1);
+        if (next != null) {
+          total++;
+          int nextX = next[0];
+          int nextY = next[1];
+          xq.add(nextX);
+          yq.add(nextY);
+          grid[nextX][nextY] = true;
+        }
+
+        next = isComfortable(curX+1, curY);
+        if (next != null) {
+          total++;
+          int nextX = next[0];
+          int nextY = next[1];
+          xq.add(nextX);
+          yq.add(nextY);
+          grid[nextX][nextY] = true;
+        }
+
+        next = isComfortable(curX-1, curY);
+        if (next != null) {
+          total++;
+          int nextX = next[0];
+          int nextY = next[1];
+          xq.add(nextX);
+          yq.add(nextY);
+          grid[nextX][nextY] = true;
+        }
+      }
+      System.out.println(total);
+    }
+    sc.close();
+  }
+
+  private static int[] isComfortable(int x, int y) {
+    if (!grid[x][y]) return null;
+    int nextX = -1;
+    int nextY = -1;
+    int around = 0;
+
+    if (!grid[x][y+1]) {nextX = x; nextY = y+1;}
+    else around++;
+    if (!grid[x][y-1]) {nextX = x; nextY = y-1;}
+    else around++;
+    if (!grid[x+1][y]) {nextX = x+1; nextY = y;}
+    else around++;
+    if (!grid[x-1][y]) {nextX = x-1; nextY = y;}
+    else around++;
+
+    int[] result = new int[2];
+
+    if (around == 3) {
+      result[0] = nextX;
+      result[1] = nextY;
+      return result;
+    }
+    return null;
+  }
+}
