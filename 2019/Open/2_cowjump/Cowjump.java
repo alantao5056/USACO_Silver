8bc21b035a8012a435ec03e0ff3a6d01752bfde0
alantao5056 <alantao5056@outlook.com>
2022-07-19 10:36:41 -0400

    finished sort

---
+++ 2019/Open/2_cowjump/Cowjump.java
@@ -0,0 +1,21 @@
+import java.io.PrintWriter;
+import java.io.File;
+import java.util.*;
+import java.awt.geom;
+
+public class Cowjump {
+  static int N;
+
+  public static void main(String[] args) throws Exception {
+    // read input
+    Scanner sc = new Scanner(new File("cowjump.in"));
+    PrintWriter pw = new PrintWriter("cowjump.out");
+    N = sc.nextInt();
+    
+    // solve
+    
+
+    sc.close();
+    pw.close();
+  }
+}
